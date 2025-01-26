f1d4e6bff5e882268c9c0d146a91997cb1ce3745
Ali Ghanad <sherkata714@gmail.com>
2025-01-26 17:12:15 +0330

    add whishlist page

--- src/Components/ProductSlider/ProductSlider.jsx
+++ src/Components/ProductSlider/ProductSlider.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
@@ -6,16 +6,19 @@ import { IoIosHeartEmpty } from "react-icons/io";
 import { TiStarFullOutline } from "react-icons/ti";
 import { FaStarHalf } from "react-icons/fa6";
 
-const ProductSlider = ({ products , setup }) => {
+const ProductSlider = ({ products, setup }) => {
+    const whishlistProducts = [];
+    console.log('whishlistArray', whishlistProducts);
 
-    const starRate = (rate)=>{
+    const starRate = (rate) => {
         const stars = [];
-        for(let i=1; i<=rate;i++){
-            stars.push(<TiStarFullOutline/>)
+        for (let i = 1; i <= rate; i++) {
+            stars.push(<TiStarFullOutline />)
         }
 
-        return <div style={{color:"#FFAD33"}} className='flex'>{stars.map(item=>(<div>{item}</div>))}{rate>Math.floor(rate) && <FaStarHalf/>}</div>
+        return <div style={{ color: "#FFAD33" }} className='flex'>{stars.map(item => (<div>{item}</div>))}{rate > Math.floor(rate) && <FaStarHalf />}</div>
     }
+
 
     return (
         <div className='w-full'>
@@ -26,8 +29,15 @@ const ProductSlider = ({ products , setup }) => {
                             <div id='productPic' className='w-[250px] h-[250px] relative'>
                                 <img className='h-full object-scale-down mx-auto' src={item.image} />
                                 <div className='absolute top-2 w-full px-2 flex items-center justify-between'>
-                                    <p style={{ boxShadow: "2px 2px 4px 0px rgba(0, 0, 0, 0.363)",display:`${setup.rows ? "none":"block"}` }} className='px-2 bg-red-600 rounded-md text-white w-fit'>- {item.category === "jewelery" ? "85%" : "50%"}</p>
-                                    <div style={{ backgroundColor: "rgba(172, 188, 202, 0.252)" }} className='w-[30px] h-[30px] rounded-full flex items-center justify-center'>
+                                    <p style={{ boxShadow: "2px 2px 4px 0px rgba(0, 0, 0, 0.363)", display: `${setup.rows ? "none" : "block"}` }} className='px-2 bg-red-600 rounded-md text-white w-fit'>- {item.category === "jewelery" ? "85%" : "50%"}</p>
+                                    <div onClick={() => {
+                                        whishlistProducts.includes(item) ? null : whishlistProducts.push(item)
+                                        console.log(whishlistProducts);
+                                        localStorage.setItem("whishlistProducts", JSON.stringify(whishlistProducts))
+                                    }}
+                                        style={{ backgroundColor: "rgba(172, 188, 202, 0.252)" }}
+                                        className='w-[30px] h-[30px] rounded-full flex items-center justify-center'
+                                    >
                                         <IoIosHeartEmpty className='text-[20px]' />
                                     </div>
                                 </div>
@@ -37,8 +47,8 @@ const ProductSlider = ({ products , setup }) => {
                                 </div>
                             </div>
 
-                            <div style={{backgroundColor:"rgba(172, 188, 202, 0.12)"}} className='p-2 h-[100px] flex flex-col justify-between rounded-md'>
-                                <p>{item.title.slice(0,26)}{item.title.length > 27 && "..."}</p>
+                            <div style={{ backgroundColor: "rgba(172, 188, 202, 0.12)" }} className='p-2 h-[100px] flex flex-col justify-between rounded-md'>
+                                <p>{item.title.slice(0, 26)}{item.title.length > 27 && "..."}</p>
                                 <p className='text-red-600'>${item.price}</p>
                                 <p className='flex gap-x-2'>{starRate(item.rating.rate)} <span className='text-[12px] text-slate-400'>({item.rating.rate})</span></p>
                             </div>
--- src/Pages/Whishlist/Whishlist.jsx
+++ src/Pages/Whishlist/Whishlist.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import ProductSlider from '../../Components/ProductSlider/ProductSlider'
 
 const Whishlist = () => {
+  let Arrowsettings = {
+    dots: false,
+    arrows: true,
+    infinite: true,
+    focusOnSelect: false,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />
+  };
   return (
     <div>
-      whishlist
+      <ProductSlider products={whishlistProduct} setup={Arrowsettings} />
     </div>
   )
 }
